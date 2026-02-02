1cffe19b4ae48b52ea4bb56defd0d1c1e46d7673
Abdulrahim Jamil <jameelangoabdulraheem@gmail.com>
2026-02-02 12:50:32 +0100

    fixed hydration bug

--- utils/context.jsx
+++ utils/context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect, useCallback } from "react";
 import { hydrateUser, LogOut } from "./fetch";
-import { isAfter } from "date-fns";
+import { isAfter, addDays } from "date-fns";
 import { toast } from "sonner";
 
 export const AuthContext = createContext();
@@ -14,31 +14,13 @@ export function AuthProvider({ children }) {
    * Runs the external hydration function and updates user state.
    */
 
-  function checkUser(user){
-    if(import.meta.env.VITE_NODE_ENV === "production"){
-      if(isAfter(new Date(), addDays(user.created, 50))) {
-        toast.error("Free trial has ended. Contact DevAbdul for more info");
-        return true;
-      }else{
-        return false;
-      }
-    }else{
-      return false;
-    }
-  }
-
   const hydrate = useCallback(async () => {
     setUserLoad(true);
     setError(null);
 
     try{
       const data = await hydrateUser();
-      if(!checkUser(data.user)){
-        setUser(null);
-        return;
-      }else{
-        setUser(data.user);
-      }
+      setUser(data.user);
       
     }catch(err){
       console.error("Auth hydration failed:", err);
